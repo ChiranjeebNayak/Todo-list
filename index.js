d12dfc2e2df30c73e7ed24dd04d59627aab70ed3
Chiranjeeb Nayak <63226358+ChiranjeebNayak@users.noreply.github.com>
2020-12-30 22:57:54 +0530

    update for refresh retrieve data feature

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+var temp =new Map();
+var temp1 =new Map();
+
 function onpageload(){
 
 var d = new Date();
 var weekday = new Array(7);
+
 weekday[0] = "Sunday";
 weekday[1] = "Monday";
 weekday[2] = "Tuesday";
@@ -17,21 +21,18 @@ var month =d.getMonth();
 var year =d.getFullYear();
 str=str+n+" "+date+"/"+month+"/"+year;
 document.getElementById('date').innerHTML=str;
+//localStorage.setItem("toDoListLenght",0);
+//localStorage.setItem("completedListLength",0);
+//localStorage.clear();
 if (typeof(Storage) !== "undefined") {
-    // Store
-localStorage.setItem("todolsname", "Chiranjeeb Nayak");
-// Retrieve
-console.log(localStorage.getItem("todolsname"));
-var dataObject = { 'item1': 1, 'item2': 2, 'item3': 3 };
-
-// Set localStorage item
-localStorage.setItem('dataObject', JSON.stringify(dataObject));
-
-// Retrieve the object from localStorage
-var retrievedObject = localStorage.getItem('dataObject');
-
-// console.log retrieved item
-console.log('retrieved data Object: ', JSON.parse(retrievedObject));
+  if (localStorage.getItem("toDoListLenght") === null) {
+    localStorage.setItem("toDoListLenght",0);
+    localStorage.setItem("completedListLength",0);
+  }
+  else{
+    setTableAfterRefresh();
+    setCompletedTableAfterRefresh();
+  }
   } else {
     alert("Sorry! No Web Storage support..");
   }
@@ -50,16 +51,36 @@ function addtask(){
         cell3.innerHTML = "<button class='btn-sm btn-success'onclick='completetask(this)'>Complete</button>";
         cell4.innerHTML = "<button class='btn-sm btn-danger'onclick='deletetask(this)'>Delete</button>";
         document.getElementById('task').innerHTML="";
+
+        localStorage.setItem("toDoListLenght",parseInt(localStorage.getItem("toDoListLenght"))+1);
+        localStorage.setItem("item"+parseInt(localStorage.getItem("toDoListLenght")),task);
+        temp["item"+parseInt(localStorage.getItem("toDoListLenght"))]=task;
+
+        document.getElementById("task").value="";
+
     }
 
 function  deletetask(element) {
+  
+   
     var row = element.closest('tr').rowIndex;
     var column = element.closest('td').cellIndex;
     console.log("row = "+row +" column = "+" "+column);
     var table = document.getElementById("myTable");
     var x=table.rows[row].cells.item(column-1).innerHTML;
+    var keyval=table.rows[row].cells.item(1).innerHTML;
+    console.log("..key: ",keyval);
     console.log(x);
     table.deleteRow(row);
+
+    
+    var key1 = getKeyByValue(keyval);
+    console.log("...key1: "+key1)
+    localStorage.removeItem(key1);
+    temp.delete(key1);
+  
+    
+    
 }
 
   function completetask(element){
@@ -78,6 +99,17 @@ function  deletetask(element) {
     cell2.innerHTML = "<td>"+task+"</td>";
     cell3.innerHTML = "<td><input type='button' class='btn-sm btn-primary ' onclick='undotask(this)' value='Undo'></td>";
     table.deleteRow(rowno);
+
+
+    console.log(parseInt("comp"+localStorage.getItem("completedListLength"))+1+"  ....."+task);
+    localStorage.setItem("completedListLength",parseInt(localStorage.getItem("completedListLength"))+1);
+    localStorage.setItem("comp"+parseInt(localStorage.getItem("completedListLength")),task);
+    temp1["comp"+parseInt(localStorage.getItem("completedListLength"))]=task;
+
+    var key1 = getKeyByValue(task);
+    console.log("...key1: "+key1)
+    localStorage.removeItem(key1);
+    temp.delete(key1);
     
 
   }
@@ -98,5 +130,146 @@ function  deletetask(element) {
         cell3.innerHTML = "<button class='btn-sm btn-success'onclick='completetask(this)'>Complete</button>";
         cell4.innerHTML = "<button class='btn-sm btn-danger'onclick='deletetask(this)'>Delete</button>";
         completetable.deleteRow(rowno);
+
+
+
+        localStorage.setItem("toDoListLenght",parseInt(localStorage.getItem("toDoListLenght"))+1);
+        localStorage.setItem("item"+parseInt(localStorage.getItem("toDoListLenght")),task);
+        temp["item"+parseInt(localStorage.getItem("toDoListLenght"))]=task;
+
+        var key1 = getKeyByValue1(task);
+        console.log("...key1: "+key1)
+        localStorage.removeItem(key1);
+        temp1.delete(key1);
+
   }
 
+
+  function setTableAfterRefresh()
+  {
+
+    if (typeof(Storage) !== "undefined") {
+  //     // Store
+  // localStorage.setItem("todolsname", "toDoList");
+  // // Retrieve
+  // console.log(localStorage.getItem("listLength"));
+   var len = parseInt(localStorage.getItem("toDoListLenght"));
+   console.log("length  "+len);
+  for(var i=0;i<=len;i++)
+  {
+    var listItem = localStorage.getItem("item"+i);
+    if(listItem!=null)
+    {
+      addtask2(listItem);
+      temp["item"+i]=listItem;
+      console.log(listItem);
+    }
+   
+  }
+//  var dataObject = { 'item1': 1, 'item2': 2, 'item3': 3 };
+  
+  // Set localStorage item
+//  localStorage.setItem('dataObject', JSON.stringify(dataObject));
+  
+  // Retrieve the object from localStorage
+//  var retrievedObject = localStorage.getItem('dataObject');
+  
+  // console.log retrieved item
+//  console.log('retrieved data Object: ', JSON.parse(retrievedObject));
+    } else {
+      alert("Sorry! No Web Storage support..");
+    }
+
+  }
+
+  function addtask2(task){
+    var table = document.getElementById("myTable");
+    var row = table.insertRow(0);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    var cell4 = row.insertCell(3);
+    cell1.innerHTML = "<td> <i class='material-icons'>assignment</i></td>";
+    cell2.innerHTML = task;
+    cell3.innerHTML = "<button class='btn-sm btn-success'onclick='completetask(this)'>Complete</button>";
+    cell4.innerHTML = "<button class='btn-sm btn-danger'onclick='deletetask(this)'>Delete</button>";
+    document.getElementById('task').innerHTML="";
+}
+
+function getKeyByValue(searchValue) {
+ 
+  for (var [key, value] of Object.entries(temp)) {
+  
+    console.log(key+"  "+value);
+    if (value === searchValue)
+      return key;
+  }
+}
+
+function getKeyByValue1(searchValue) {
+ 
+  for (var [key, value] of Object.entries(temp1)) {
+  
+    console.log(key+"  "+value);
+    if (value === searchValue)
+      return key;
+  }
+}
+
+function setCompletedTableAfterRefresh()
+{
+
+  if (typeof(Storage) !== "undefined") {
+//     // Store
+// localStorage.setItem("todolsname", "toDoList");
+// // Retrieve
+// console.log(localStorage.getItem("listLength"));
+ var len = parseInt(localStorage.getItem("completedListLength"));
+ console.log("completed length  "+len);
+for(var i=0;i<=len;i++)
+{
+  var listItem = localStorage.getItem("comp"+i);
+  if(listItem!=null)
+  {
+    completetask2(listItem);
+    temp1["comp"+i]=listItem;
+    console.log(listItem);
+  }
+ 
+}
+//  var dataObject = { 'item1': 1, 'item2': 2, 'item3': 3 };
+
+// Set localStorage item
+//  localStorage.setItem('dataObject', JSON.stringify(dataObject));
+
+// Retrieve the object from localStorage
+//  var retrievedObject = localStorage.getItem('dataObject');
+
+// console.log retrieved item
+//  console.log('retrieved data Object: ', JSON.parse(retrievedObject));
+  } else {
+    alert("Sorry! No Web Storage support..");
+  }
+
+}
+
+
+function completetask2(task){
+
+  var completetable = document.getElementById("completetable");
+
+ 
+  
+  var row = completetable.insertRow(0);
+  var cell1 = row.insertCell(0);
+  var cell2 = row.insertCell(1);
+  var cell3 = row.insertCell(2);
+  cell1.innerHTML = "<td> <i class='material-icons'>assignment_turned_in</i></td>";
+  cell2.innerHTML = "<td>"+task+"</td>";
+  cell3.innerHTML = "<td><input type='button' class='btn-sm btn-primary ' onclick='undotask(this)' value='Undo'></td>";
+
+
+
+  
+
+}
